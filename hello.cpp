0f40ea83a54cf7e5a9549cc45266496e52b1510f
wraptneutralino9 <yukakosally3502@outlook.com>
2025-10-19 06:52:12 -0400

    Add hello.cpp to project

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+/**
+ * CMake Project Starter - Implementation file
+ * Simple function that prints a message
+ */
+
+#include "hello.h"
+#include <iostream>
+
+void sayHello() {
+    std::cout << "Hello from hello.cpp!" << std::endl;
+}
